8a0cb90130be82cb88a0f1612103de006c667fc4
robajillo <rjillo@yahoo.com>
2020-10-24 16:48:18 +0300

    imported required file

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
