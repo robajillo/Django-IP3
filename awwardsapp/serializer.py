bbc22f194d21fe4d26e6147103a7d356f85ed211
robajillo <rjillo@yahoo.com>
2020-10-24 22:49:26 +0300

    created class PostSerializer

--- awwardsapp/serializer.py
+++ awwardsapp/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id','user','profile_picture','bio','name','location','contact')
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Post
+        fields =('id','title','project_link', 'description','profile','image','user','created_date')
