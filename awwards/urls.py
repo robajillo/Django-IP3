53088dacf6a137231427d62a4c3573fa0be3a5f2
robajillo <rjillo@yahoo.com>
2020-10-24 16:04:14 +0300

    added authentication path

--- awwards/urls.py
+++ awwards/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awwardsapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
 ]
