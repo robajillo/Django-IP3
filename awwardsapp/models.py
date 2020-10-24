12973f6bd9df8ef8490c04fa7f43f42827111661
robajillo <rjillo@yahoo.com>
2020-10-24 16:08:39 +0300

    imported current user field

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -2,4 +2,5 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_currentuser.db.models import CurrentUserField
 # Create your models here.
