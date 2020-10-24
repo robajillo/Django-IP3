34bf2a3f68c6d8ea7ced5109a75ffd5bc01e5a89
robajillo <rjillo@yahoo.com>
2020-10-24 16:09:20 +0300

    imported post_save

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -3,4 +3,5 @@ import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django_currentuser.db.models import CurrentUserField
+from django.db.models.signals import post_save
 # Create your models here.
