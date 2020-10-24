da3037b808dc1d7d65a8e7372b5f13beb18884bc
robajillo <rjillo@yahoo.com>
2020-10-24 22:42:51 +0300

    created class profilelist

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -112,3 +112,19 @@ def search_results(request):
     else:
         message = "You haven't searched for any project"
         return render(request,'search.html',{"message":message})
+
+
+class ProfileList(APIView):
+
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        permission_classes = (IsAdminOrReadOnly,)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
