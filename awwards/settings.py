cd5042b10462949d7b33e7885fc54f3dbd19f270
robajillo <rjillo@yahoo.com>
2020-10-24 16:00:41 +0300

    changed timezone

--- awwards/settings.py
+++ awwards/settings.py
@@ -92,15 +92,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'awwards.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -127,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
