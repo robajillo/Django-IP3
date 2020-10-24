38e236d80cb9d9378573c92a6285c527897fd142
robajillo <rjillo@yahoo.com>
2020-10-24 16:10:04 +0300

    imported reciever and ImageField

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -4,4 +4,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django_currentuser.db.models import CurrentUserField
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+from pyuploadcare.dj.models import ImageField
 # Create your models here.
