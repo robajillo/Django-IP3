0a24f94b84140cbaccc3ad022539e8547a8ce0e0
robajillo <rjillo@yahoo.com>
2020-10-24 16:19:20 +0300

    created class Profile

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -6,4 +6,25 @@ from django_currentuser.db.models import CurrentUserField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from pyuploadcare.dj.models import ImageField
-# Create your models here.
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
