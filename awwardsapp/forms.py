a989a6aae7345ecc12ea73345c8e81a1be0fa793
robajillo <rjillo@yahoo.com>
2020-10-24 16:54:12 +0300

    created NewProfileForm

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -2,3 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
