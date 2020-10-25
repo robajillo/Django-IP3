b4372660170a195a9cd449dd11048f3ccfc50ca0
robajillo <rjillo@yahoo.com>
2020-10-25 10:53:32 +0300

    installed environment

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django_currentuser.db.models import CurrentUserField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from pyuploadcare.dj.models import ImageField
+
 
 
 
